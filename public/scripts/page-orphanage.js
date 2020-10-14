d78ad2c463b9dbabf114b17b86b1ff9fd417865a
Otavio Silva <otavioolsilva@outlook.com>
2020-10-14 15:50:50 -0300

    Dia 02

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,39 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+//create map
+const map = L.map('mapid', options).setView([-27.2163379, -49.6546252], 14);
+
+//create tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+//create and add marker
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+L.marker([-27.2163379, -49.6546252], { icon }).addTo(map);
+
+//image gallery
+function selectImage(event) {
+  const button = event.currentTarget;
+
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
+  imageContainer.src = image.src;
+
+  button.classList.add("active");
+}
